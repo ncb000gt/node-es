ba59efdd0a28f30b81f999ef52cc1982963f03da
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-03-08 17:37:29 -0500

    Queries are less complicated by just using the JSON DSL. No need for complicated abstractions.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var elasticsearch = require('./lib/elasticsearch'),
     cluster = require('./lib/cluster'),
-    query = require('./lib/query'),
     utils = require('./lib/utils'),
     exports = module.exports;
 
 exports.createClient = function() {
     return new elasticsearch(arguments);
 }
-exports.query = query;
 exports.cluster = cluster;
